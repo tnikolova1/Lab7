5a9abda786d04ea82dd08ab356e4fdd88619639b
Teodora <tnikolova@hawk.iit.edu>
2019-11-19 16:18:09 -0600

    uploading updated js file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  var msg = document.getElementById('entryinput');
+  var eb = document.getElementById('entrybutton');
+  
+  eb.addEventListener('click', function() {
+   alert('Teodora Nikolova: ' +msg.value);
+   document.getElementById('textoutput').innerHTML = msg.value;});
+}
 window.addEventListener('load', init);
